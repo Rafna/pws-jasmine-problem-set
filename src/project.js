d630342d49d3e624689cb00a0012fbe46a7c261a
Rafna <rafnabash@gmail.com>
2017-10-16 20:48:32 -0600

    Velocity calculated for each iteration

--- src/project.js
+++ src/project.js
@@ -1,10 +1,14 @@
 function Project() {
+	Iteration.call(this);
+	this.iteration = 0;
 }
 
 Project.prototype.addIteration = function(iteration) {
+	this.iteration += iteration;
 }
 
 Project.prototype.velocity = function() {
+	return Iteration.prototype.totalPoints.call(this)/this.iteration;
 }
 
 

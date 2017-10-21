be9c53f8dccdaa500773bf252c3156ca77a3de4c
Rafna <rafnabash@gmail.com>
2017-10-21 00:26:44 -0600

    Third test passed

--- src/iteration.js
+++ src/iteration.js
@@ -1,10 +1,8 @@
 function Iteration() {
-	this.pointsArray = [];
     this.points = 0;
 }
 
 Iteration.prototype.addPoints = function(points) {
-	this.pointsArray.push(points);
 	this.points += points;
 };
 
--- src/project.js
+++ src/project.js
@@ -1,13 +1,22 @@
 function Project() {
 	this.iterationCount = [];
 	this.totalIterationPoints = 0;
+	this.iterationArray = [];
 }
 Project.prototype.addIteration = function(iteration) {
 	this.iterationCount.push(iteration);
-	this.totalIterationPoints += iteration.totalPoints();
+	if(this.iterationCount.length > 3){
+		this.iterationCount.splice(0,1);
+		this.totalIterationPoints += iteration.totalPoints() - this.iterationArray[0];
+	}
+	else{
+		this.totalIterationPoints += iteration.totalPoints();
+		this.iterationArray.push(this.totalIterationPoints);
+	}
 };
 
 Project.prototype.velocity = function() {
 	return this.totalIterationPoints / this.iterationCount.length;
 }
+
 
